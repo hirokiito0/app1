f9d47a735d9db372d024aa27d4e81949aa78d595
EC2 Default User <ec2-user@ip-172-31-88-243.ec2.internal>
2021-06-06 11:03:05 +0000

    [start]4-7_削除機能_destroyアクション追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -27,7 +27,10 @@ class BlogsController < ApplicationController
     blog = Blog.find(params[:id])
     blog.update(blog_params)
     redirect_to blog_path(blog)
-  end 
+  end
+  
+  def destroy
+    
    
    private
   def blog_params
